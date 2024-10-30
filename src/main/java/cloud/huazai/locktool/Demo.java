d57076cccb898099e45ff591ca6ab06b80e5984d
HuaZai <huazai@huazai.cloud>
2024-10-30 13:27:12 +0800

    测试

---
+++ src/main/java/cloud/huazai/locktool/Demo.java
@@ -0,0 +1,13 @@
+package cloud.huazai.locktool;
+
+/**
+ * Demo
+ *
+ * @author devon
+ * @date 2024/10/30
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
